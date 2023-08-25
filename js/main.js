213224dd6fcc0c3fa4a59486ad808a468177c4df
mahkamovdiyorbek <mahkamovdiyorbek954@gmail.com>
2023-08-25 17:56:36 +0500

    JS 3-dars
    26-guruh o'quvchisi

---
+++ js/main.js
@@ -0,0 +1,34 @@
+/* Math in JS*/
+let round = Math.round(8.4)   /* Yaxlidlash-1 */
+let pow = Math.pow(4,4)     /* Darajasini Hisoblash */
+let sqrt = Math.sqrt(35)     /* Ildizini topish */
+let abs = Math.abs(-12.8)    /* Absalut sonni aniqlash */
+let ceil = Math.ceil(49.1)   /* Yaxlidlash-2*/
+let floor = Math.floor(49.9)    /* Yaxlidlash-3*/
+let trunc = Math.trunc(49.9)   /* Yaxlidlash-4*/
+let min = Math.min(25,17,122,779,31,47)  /* Minimal sonni topish*/
+let max = Math.max(25,17,122,779,31,47)  /* Maximal sonni topish*/
+
+document.write(max)
+
+
+/* DOM and styles in JS*/
+
+/*let byTag = document.getElementsByTagName("h1")*/
+/*let byId = document.getElementById("idNomi")*/
+/*let byClass = document.getElementsByClassName("classnomi")*/
+/*let bySelector = document.querySelector(".classnomi");*/
+/*let bySelectorAll = document.querySelectorAll(".classnomi");*/
+/*let byAll = document.getElementsByTagName("h1").namedItem("idNomi");*/
+
+/*byAll.style.color = "yellow"*/
+/*byAll.style.backgroundColor = "blue"*/
+/*byAll.style.width = "300px"*/
+/*byAll.style.height = "250px"*/
+/*byAll.style.fontSize = "45px"*/
+/*byAll.style.paddingTop = "90px"*/
+/*byAll.style.boxSizing = "border-box"*/
+/*byAll.style.textAlign = "center"*/
+/*byAll.style.border = "10px solid green"*/
+
+/*console.log(byAll);*/
---
+++ js/main1.js
@@ -0,0 +1,328 @@
+let byWrap = document.getElementsByTagName("div").namedItem("wrap")
+byWrap.style.width = "1200px"
+byWrap.style.margin = "0 auto"
+
+let byFormGroup = document.getElementsByTagName("div").namedItem("form-group")
+byFormGroup.style.width = "500px"
+byFormGroup.style.margin = "0 auto"
+byFormGroup.style.boxShadow = "0 0 10px rgba(80,80,80,0.5)"
+byFormGroup.style.padding = "1rem 1.2rem"
+byFormGroup.style.boxSizing = "border-box"
+
+let byInput1 = document.getElementsByTagName("input").namedItem("firstName")
+let byInput2 = document.getElementsByTagName("input").namedItem("lastName")
+let byInput3 = document.getElementsByTagName("input").namedItem("email")
+let byInput4 = document.getElementsByTagName("input").namedItem("password")
+let byInput5 = document.getElementsByTagName("input").namedItem("color")
+let byInput6 = document.getElementsByTagName("input").namedItem("week")
+let byInput7 = document.getElementsByTagName("input").namedItem("month")
+let byInput8 = document.getElementsByTagName("input").namedItem("date")
+let byInput9 = document.getElementsByTagName("input").namedItem("time")
+let byInput10 = document.getElementsByTagName("input").namedItem("ha")
+let byInput11 = document.getElementsByTagName("input").namedItem("yoq")
+let byInput12 = document.getElementsByTagName("input").namedItem("bilmadim")
+let byInput13 = document.getElementsByTagName("input").namedItem("ha2")
+let byInput14 = document.getElementsByTagName("input").namedItem("yoq2")
+let byInput15 = document.getElementsByTagName("input").namedItem("bilmadim2")
+let byInput16 = document.getElementsByTagName("input").namedItem("submit")
+byInput1.style.width = "60%"
+byInput1.style.padding = "0.6rem 1.5rem"
+byInput1.style.boxSizing = "border-box"
+byInput1.style.border = "1px solid #cccccc"
+byInput1.style.borderRadius = "4px"
+byInput1.style.display = "inline-block"
+byInput1.style.marginBottom = "10px"
+
+byInput2.style.width = "60%"
+byInput2.style.padding = "0.6rem 1.5rem"
+byInput2.style.boxSizing = "border-box"
+byInput2.style.border = "1px solid #cccccc"
+byInput2.style.borderRadius = "4px"
+byInput2.style.display = "inline-block"
+byInput2.style.marginBottom = "15px"
+
+byInput3.style.width = "60%"
+byInput3.style.padding = "0.6rem 1.5rem"
+byInput3.style.boxSizing = "border-box"
+byInput3.style.border = "1px solid #cccccc"
+byInput3.style.borderRadius = "4px"
+byInput3.style.display = "inline-block"
+byInput3.style.marginBottom = "10px"
+
+byInput4.style.width = "60%"
+byInput4.style.padding = "0.6rem 1.5rem"
+byInput4.style.boxSizing = "border-box"
+byInput4.style.border = "1px solid #cccccc"
+byInput4.style.borderRadius = "4px"
+byInput4.style.display = "inline-block"
+byInput4.style.marginBottom = "50px"
+
+byInput5.style.padding = "0"
+
+byInput6.style.width = "60%"
+byInput6.style.padding = "0.6rem 1.5rem"
+byInput6.style.boxSizing = "border-box"
+byInput6.style.border = "1px solid #cccccc"
+byInput6.style.borderRadius = "4px"
+byInput6.style.display = "inline-block"
+byInput6.style.marginBottom = "10px"
+
+byInput7.style.width = "60%"
+byInput7.style.padding = "0.6rem 1.5rem"
+byInput7.style.boxSizing = "border-box"
+byInput7.style.border = "1px solid #cccccc"
+byInput7.style.borderRadius = "4px"
+byInput7.style.display = "inline-block"
+byInput7.style.marginBottom = "10px"
+
+byInput8.style.width = "60%"
+byInput8.style.padding = "0.6rem 1.5rem"
+byInput8.style.boxSizing = "border-box"
+byInput8.style.border = "1px solid #cccccc"
+byInput8.style.borderRadius = "4px"
+byInput8.style.display = "inline-block"
+byInput8.style.marginBottom = "10px"
+
+byInput9.style.width = "60%"
+byInput9.style.padding = "0.6rem 1.5rem"
+byInput9.style.boxSizing = "border-box"
+byInput9.style.border = "1px solid #cccccc"
+byInput9.style.borderRadius = "4px"
+byInput9.style.display = "inline-block"
+byInput9.style.marginBottom = "10px"
+
+byInput10.style.width = "60%"
+byInput10.style.padding = "0.6rem 1.5rem"
+byInput10.style.boxSizing = "border-box"
+byInput10.style.border = "1px solid #cccccc"
+byInput10.style.borderRadius = "4px"
+byInput10.style.display = "inline-block"
+byInput10.style.marginBottom = "10px"
+
+byInput11.style.width = "60%"
+byInput11.style.padding = "0.6rem 1.5rem"
+byInput11.style.boxSizing = "border-box"
+byInput11.style.border = "1px solid #cccccc"
+byInput11.style.borderRadius = "4px"
+byInput11.style.display = "inline-block"
+byInput11.style.marginBottom = "10px"
+
+byInput12.style.width = "60%"
+byInput12.style.padding = "0.6rem 1.5rem"
+byInput12.style.boxSizing = "border-box"
+byInput12.style.border = "1px solid #cccccc"
+byInput12.style.borderRadius = "4px"
+byInput12.style.display = "inline-block"
+byInput12.style.marginBottom = "10px"
+
+byInput13.style.width = "60%"
+byInput13.style.padding = "0.6rem 1.5rem"
+byInput13.style.boxSizing = "border-box"
+byInput13.style.border = "1px solid #cccccc"
+byInput13.style.borderRadius = "4px"
+byInput13.style.display = "inline-block"
+byInput13.style.marginBottom = "10px"
+
+byInput14.style.width = "60%"
+byInput14.style.padding = "0.6rem 1.5rem"
+byInput14.style.boxSizing = "border-box"
+byInput14.style.border = "1px solid #cccccc"
+byInput14.style.borderRadius = "4px"
+byInput14.style.display = "inline-block"
+byInput14.style.marginBottom = "10px"
+
+byInput15.style.width = "60%"
+byInput15.style.padding = "0.6rem 1.5rem"
+byInput15.style.boxSizing = "border-box"
+byInput15.style.border = "1px solid #cccccc"
+byInput15.style.borderRadius = "4px"
+byInput15.style.display = "inline-block"
+byInput15.style.marginBottom = "10px"
+
+byInput16.style.width = "200px"
+byInput16.style.padding = "0.5rem 1.5rem"
+byInput16.style.display = "block"
+byInput16.style.margin = "20px auto"
+byInput16.style.backgroundColor = "#45bd42"
+byInput16.style.color = "#ffffff"
+
+
+let byLabel1 = document.getElementsByTagName("label").namedItem("label1")
+let byLabel2  = document.getElementsByTagName("label").namedItem("label2")
+let byLabel3  = document.getElementsByTagName("label").namedItem("label3")
+let byLabel4  = document.getElementsByTagName("label").namedItem("label4")
+let byLabel5  = document.getElementsByTagName("label").namedItem("label5")
+let byLabel6  = document.getElementsByTagName("label").namedItem("label6")
+let byLabel7  = document.getElementsByTagName("label").namedItem("label7")
+let byLabel8  = document.getElementsByTagName("label").namedItem("label8")
+let byLabel9  = document.getElementsByTagName("label").namedItem("label9")
+let byLabel10  = document.getElementsByTagName("label").namedItem("label10")
+let byLabel11 = document.getElementsByTagName("label").namedItem("label11")
+let byLabel12 = document.getElementsByTagName("label").namedItem("label12")
+let byLabel13 = document.getElementsByTagName("label").namedItem("label13")
+let byLabel14 = document.getElementsByTagName("label").namedItem("label14")
+let byLabel15 = document.getElementsByTagName("label").namedItem("label15")
+let byLabel16 = document.getElementsByTagName("label").namedItem("label16")
+let byLabel17 = document.getElementsByTagName("label").namedItem("label17")
+let byLabel18 = document.getElementsByTagName("label").namedItem("label18")
+byLabel1.style.width = "35%"
+byLabel1.style.padding = "0.6rem 1.5rem"
+byLabel1.style.boxSizing = "border-box"
+byLabel1.style.display = "inline-block"
+byLabel1.style.marginBottom = "10px"
+byLabel1.style.textAlign = "right"
+
+byLabel2.style.width = "35%"
+byLabel2.style.padding = "0.6rem 1.5rem"
+byLabel2.style.boxSizing = "border-box"
+byLabel2.style.display = "inline-block"
+byLabel2.style.marginBottom = "10px"
+byLabel2.style.textAlign = "right"
+
+byLabel3.style.width = "35%"
+byLabel3.style.padding = "0.6rem 1.5rem"
+byLabel3.style.boxSizing = "border-box"
+byLabel3.style.display = "inline-block"
+byLabel3.style.marginBottom = "10px"
+byLabel3.style.textAlign = "right"
+
+byLabel4.style.width = "35%"
+byLabel4.style.padding = "0.6rem 1.5rem"
+byLabel4.style.boxSizing = "border-box"
+byLabel4.style.display = "inline-block"
+byLabel4.style.marginBottom = "10px"
+byLabel4.style.textAlign = "right"
+
+byLabel5.style.width = "35%"
+byLabel5.style.padding = "0.6rem 1.5rem"
+byLabel5.style.boxSizing = "border-box"
+byLabel5.style.display = "inline-block"
+byLabel5.style.marginBottom = "10px"
+byLabel5.style.textAlign = "right"
+
+byLabel6.style.width = "35%"
+byLabel6.style.padding = "0.6rem 1.5rem"
+byLabel6.style.boxSizing = "border-box"
+byLabel6.style.display = "inline-block"
+byLabel6.style.marginBottom = "10px"
+byLabel6.style.textAlign = "right"
+
+byLabel7.style.width = "35%"
+byLabel7.style.padding = "0.6rem 1.5rem"
+byLabel7.style.boxSizing = "border-box"
+byLabel7.style.display = "inline-block"
+byLabel7.style.marginBottom = "10px"
+byLabel7.style.textAlign = "right"
+
+byLabel8.style.width = "35%"
+byLabel8.style.padding = "0.6rem 1.5rem"
+byLabel8.style.boxSizing = "border-box"
+byLabel8.style.display = "inline-block"
+byLabel8.style.marginBottom = "10px"
+byLabel8.style.textAlign = "right"
+
+byLabel9.style.width = "35%"
+byLabel9.style.padding = "0.6rem 1.5rem"
+byLabel9.style.boxSizing = "border-box"
+byLabel9.style.display = "inline-block"
+byLabel9.style.marginBottom = "10px"
+byLabel9.style.textAlign = "right"
+
+byLabel10.style.width = "35%"
+byLabel10.style.padding = "0.6rem 1.5rem"
+byLabel10.style.boxSizing = "border-box"
+byLabel10.style.display = "inline-block"
+byLabel10.style.marginBottom = "10px"
+byLabel10.style.textAlign = "right"
+
+byLabel11.style.width = "35%"
+byLabel11.style.padding = "0.6rem 1.5rem"
+byLabel11.style.boxSizing = "border-box"
+byLabel11.style.display = "inline-block"
+byLabel11.style.marginBottom = "10px"
+byLabel11.style.textAlign = "right"
+
+byLabel12.style.width = "35%"
+byLabel12.style.padding = "0.6rem 1.5rem"
+byLabel12.style.boxSizing = "border-box"
+byLabel12.style.display = "inline-block"
+byLabel12.style.marginBottom = "10px"
+byLabel12.style.textAlign = "right"
+
+byLabel13.style.width = "35%"
+byLabel13.style.padding = "0.6rem 1.5rem"
+byLabel13.style.boxSizing = "border-box"
+byLabel13.style.display = "inline-block"
+byLabel13.style.marginBottom = "10px"
+byLabel13.style.textAlign = "right"
+
+byLabel14.style.width = "35%"
+byLabel14.style.padding = "0.6rem 1.5rem"
+byLabel14.style.boxSizing = "border-box"
+byLabel14.style.display = "inline-block"
+byLabel14.style.marginBottom = "10px"
+byLabel14.style.textAlign = "right"
+
+byLabel15.style.width = "35%"
+byLabel15.style.padding = "0.6rem 1.5rem"
+byLabel15.style.boxSizing = "border-box"
+byLabel15.style.display = "inline-block"
+byLabel15.style.marginBottom = "10px"
+byLabel15.style.textAlign = "right"
+
+byLabel16.style.width = "35%"
+byLabel16.style.padding = "0.6rem 1.5rem"
+byLabel16.style.boxSizing = "border-box"
+byLabel16.style.display = "inline-block"
+byLabel16.style.marginBottom = "10px"
+byLabel16.style.textAlign = "right"
+
+byLabel17.style.width = "35%"
+byLabel17.style.padding = "0.6rem 1.5rem"
+byLabel17.style.boxSizing = "border-box"
+byLabel17.style.display = "inline-block"
+byLabel17.style.marginBottom = "10px"
+byLabel17.style.textAlign = "right"
+
+byLabel18.style.width = "35%"
+byLabel18.style.padding = "0.6rem 1.5rem"
+byLabel18.style.boxSizing = "border-box"
+byLabel18.style.display = "inline-block"
+byLabel18.style.marginBottom = "10px"
+byLabel18.style.textAlign = "right"
+
+let bySelectage = document.getElementsByTagName("select").namedItem("age")
+let bySelectday = document.getElementsByTagName("select").namedItem("day")
+let bySelectoy = document.getElementsByTagName("select").namedItem("oy")
+let bySelectyear = document.getElementsByTagName("select").namedItem("year")
+bySelectage.style.width = "20%"
+bySelectage.style.padding = "0.5rem 1.5rem"
+bySelectage.style.boxSizing = "border-box"
+bySelectage.style.border = "1px solid #cccccc"
+bySelectage.style.borderRadius = "4px"
+bySelectage.style.display = "inline-block"
+
+bySelectday.style.width = "20%"
+bySelectday.style.padding = "0.5rem 1.5rem"
+bySelectday.style.boxSizing = "border-box"
+bySelectday.style.border = "1px solid #cccccc"
+bySelectday.style.borderRadius = "4px"
+bySelectday.style.display = "inline-block"
+
+bySelectoy.style.width = "20%"
+bySelectoy.style.padding = "1rem 2rem"
+bySelectoy.style.boxSizing = "border-box"
+bySelectoy.style.border = "1px solid #cccccc"
+bySelectoy.style.borderRadius = "4px"
+bySelectoy.style.display = "inline-block"
+
+bySelectyear.style.width = "20%"
+bySelectyear.style.padding = "0.5rem 1.5rem"
+bySelectyear.style.boxSizing = "border-box"
+bySelectyear.style.border = "1px solid #cccccc"
+bySelectyear.style.borderRadius = "4px"
+bySelectyear.style.display = "inline-block"
+
+let byh41 = document.getElementsByTagName("h4").namedItem("h4")
+let byh42 = document.getElementsByTagName("h4").namedItem("h41")
+let byDesc = document.getElementsByTagName("textarea").namedItem("desc")
